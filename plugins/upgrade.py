beb3a7662577cdc5a975f0953fffda9b087b33f5
BOSS1480 <yboss1480@gmail.com>
2025-07-01 00:30:25 +0300

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -6,31 +6,10 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
-    text = """**Free Plan User**
-Daily  Upload limit 2GB
-Price 0
-
-**🪙 Basic**
-Daily  Upload  limit 20GB
-Price Rs 49  ind /🌎 0.59$  per Month
-
-**⚡ Standard**
-Daily Upload limit 50GB
-Price Rs 99  ind /🌎 1.19$  per Month
-
-**💎 Pro**
-Daily Upload limit 100GB
-Price Rs 179  ind /🌎 2.16$  per Month
-
-Payment Details :-
-<b>➜ UPI ID :</b> <code>madflixofficial@axl</code>
-<b>➜ PayPal :</b> <a href='https://www.paypal.me/jishudeveloper'>Click Here</a>
-<b>➜ QR Code :</b> <a href='https://telegra.ph/QR-Payment-07-24-4'>Click Here</a>
-
-After Payment Send Screenshots Of Payment To Admin @MadflixOfficials"""
+    text = """🧑‍💻"""
     
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/calladminrobot"),
+        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/The_Joker121_bot"),
         InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
         ])
     
@@ -40,31 +19,10 @@ After Payment Send Screenshots Of Payment To Admin @MadflixOfficials"""
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-    text = """**Free Plan User**
-Daily  Upload limit 2GB
-Price 0
-
-**🪙 Basic**
-Daily  Upload  limit 20GB
-Price Rs 49  ind /🌎 0.59$  per Month
-
-**⚡ Standard**
-Daily Upload limit 50GB
-Price Rs 99  ind /🌎 1.19$  per Month
-
-**💎 Pro**
-Daily Upload limit 100GB
-Price Rs 179  ind /🌎 2.16$  per Month
-
-Payment Details :-
-<b>➜ UPI ID :</b> <code>madflixofficial@axl</code>
-<b>➜ PayPal :</b> <a href='https://www.paypal.me/jishudeveloper'>Click Here</a>
-<b>➜ QR Code :</b> <a href='https://telegra.ph/QR-Payment-07-24-4'>Click Here</a>
-
-After Payment Send Screenshots Of Payment To Admin @MadflixOfficials"""
+    text = """🧑‍💻"""
     
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/calladminrobot"),
+        [InlineKeyboardButton("🦋 Admin", url = "https://t.me/The_Joker121_bot"),
         InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
         ])
     
